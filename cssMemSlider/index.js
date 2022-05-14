e168b79cbefd54c3288267f29f78c9e9d57d4793
Dmitry Tsimoshchanka <36646929+dimtim1992@users.noreply.github.com>
2022-05-15 00:30:01 +0300

    feat: added basic task functionality (Sun, May 15, 2022 00:37:42 AM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,1 +1,23 @@
-console.log(1);
+const buttons = document.querySelectorAll(".nav-item");
+const points = document.querySelectorAll(".point");
+const img = document.querySelector(".meme");
+const text = document.querySelector(".sign");
+
+const phrases = ["Hello, kids! It's english time!", "me, when passed the test in English", "English, motherf***er, do you speak it?", "English. Y u so hard???"];
+
+buttons.forEach((item, index) => {
+    item.addEventListener("click", change);
+
+    function change() {
+        points.forEach(el => {el.classList.remove("active")});
+        points[index].classList.add("active");
+        img.classList.add("changed");
+        text.classList.add("changed");
+        setTimeout(() => {
+            img.src = `./assets/img/${index}.jpg`;
+            text.innerText = phrases[index];
+            img.classList.remove("changed");
+            text.classList.remove("changed");
+          }, 2500);
+    }
+});
